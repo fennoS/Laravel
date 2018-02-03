4fcba030a17c58a70a5b708817aa56c86bf32a93
Fenno Desktop <Fenno.sebrechts@gmail.com>
2018-02-03 21:03:16 +0100

    Stabile versie van Lidl-Crunch!

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,49 +1,33 @@
 @extends('layouts.master')
 
 @section('content')
-
     <!-- Main content -->
-    <section class="content">
-        <div class="row">
-            <div class="col-xs-12">
-                <div class="box">
-                    <div class="box-header">
-                        <h3 class="box-title">All handed in Assigments.</h3>
-                    </div>
-                    <!-- /.box-header -->
-                    <div class="box-body">
-                        <table class="table table-bordered table-striped overviews">
-                            <thead>
-                            <tr>
-                                <th class="no-sort">Question</th>
-                                <th class="no-sort">Answer</th>
-                            </tr>
-                            </thead>
-                            <tbody>
-
-                            </tbody>
-                        </table>
-
-                    </div>
-                    <hr>
-                    <div class="box-body">
-                        <table class="table table-bordered table-striped overviews">
-                            <tbody>
-                            <tr>
-
-                            </tr>
-                            </tbody>
-                        </table>
-                    </div>
-                    <!-- /.box-body -->
-                </div>
-                <!-- /.box -->
+    <div class="col-xs-12">
+        <div class="box">
+            <div class="box-header">
+                <h3 class="box-title">All handed in Assigments.</h3>
             </div>
-            <!-- /.col -->
+            <!-- /.box-header -->
+            <div class="box-body">
+                <table class="table table-bordered table-striped overviews">
+                    <thead>
+                    <tr>
+                        <th class="no-sort">Question</th>
+                        <th class="no-sort">Answer</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <td>Question</td>
+                        <td>Answer</td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+            <!-- /.box-body -->
         </div>
-        <!-- /.row -->
-    </section>
-    <!-- /.content -->
+        <!-- /.box -->
+    </div>
+    <!-- /.col -->
 
 @endsection
-
--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,11 +1,11 @@
 <header class="main-header">
 
     <!-- Logo -->
-    <a href="index2.html" class="logo">
+    <a href="home" class="logo">
         <!-- mini logo for sidebar mini 50x50 pixels -->
-        <span class="logo-mini"><b>A</b>LT</span>
+        <span class="logo-mini"><b>L</b>C!</span>
         <!-- logo for regular state and mobile devices -->
-        <span class="logo-lg"><b>Admin</b>LTE</span>
+        <span class="logo-lg"><b>Lidl</b>Crunch!</span>
     </a>
     <!-- Header Navbar -->
     <nav class="navbar navbar-static-top" role="navigation">
@@ -16,34 +16,6 @@
         <!-- Navbar Right Menu -->
         <div class="navbar-custom-menu">
             <ul class="nav navbar-nav">
-                <!-- User Account Menu -->
-                <li class="dropdown user user-menu">
-                    <!-- Menu Toggle Button -->
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                        <!-- The user image in the navbar-->
-                        <img src="adminLTE/dist/img/meme.jpg" class="user-image" alt="User Image">
-                        <!-- hidden-xs hides the username on small devices so only the image appears. -->
-                        <span class="hidden-xs">Alexander Pierce</span>
-                    </a>
-                    <ul class="dropdown-menu">
-                        <!-- The user image in the menu -->
-                        <li class="user-header">
-                            <img src="adminLTE/dist/img/meme.jpg" class="img-circle" alt="User Image">
-                            <p>
-                                Fenno S Web Developer
-                                <small>Member since 2015</small>
-                            </p>
-                        </li>
-                        <li class="user-footer">
-                            <div class="pull-left">
-                                <a href="#" class="btn btn-default btn-flat">Profile</a>
-                            </div>
-                            <div class="pull-right">
-                                <a href="#" class="btn btn-default btn-flat">Sign out</a>
-                            </div>
-                        </li>
-                    </ul>
-                </li>
                 <!-- Control Sidebar Toggle Button -->
                 <li>
                     <a href="#" data-toggle="control-sidebar"><i class="fa fa-gears"></i></a>
@@ -52,3 +24,64 @@
         </div>
     </nav>
 </header>
+<!-- Control Sidebar -->
+<aside class="control-sidebar control-sidebar-dark">
+    <!-- Create the tabs -->
+    <ul class="nav nav-tabs nav-justified control-sidebar-tabs">
+        <h4 class="text-center">Instellingen <i class="fa fa-gears"></i></h4>
+    </ul>
+    <!-- Tab panes -->
+    <div class="tab-content">
+        <!-- Home tab content -->
+        <div class="tab-pane active" id="control-sidebar-home-tab">
+            <!-- /.control-sidebar-menu -->
+            <ul class="control-sidebar-menu">
+                <li>
+                    <a href="{{ route('logout') }}"
+                       onclick="event.preventDefault(); document.getElementById('logout-form').submit();"
+                       class="btn btn-primary btn-flat">Sign out</a>
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST"
+                          style="display: none;">{{ csrf_field() }}</form>
+                    {{--<a href="javascript:;">--}}
+                        {{--<h4 class="control-sidebar-subheading">--}}
+                            {{--Custom Template Design--}}
+                            {{--<span class="pull-right-container">--}}
+                    {{--<span class="label label-danger pull-right">70%</span>--}}
+                    {{--</span>--}}
+                        {{--</h4>--}}
+
+                        {{--<div class="progress progress-xxs">--}}
+                            {{--<div class="progress-bar progress-bar-danger" style="width: 70%"></div>--}}
+                        {{--</div>--}}
+                    {{--</a>--}}
+                </li>
+            </ul>
+            <!-- /.control-sidebar-menu -->
+
+        </div>
+        <!-- /.tab-pane -->
+        <!-- Stats tab content -->
+        <div class="tab-pane" id="control-sidebar-stats-tab">Stats Tab Content</div>
+        <!-- /.tab-pane -->
+        <!-- Settings tab content -->
+        <div class="tab-pane" id="control-sidebar-settings-tab">
+            <form method="post">
+                <h3 class="control-sidebar-heading">General Settings</h3>
+
+                <div class="form-group">
+                    <label class="control-sidebar-subheading">
+                        Report panel usage
+                        <input type="checkbox" class="pull-right" checked>
+                    </label>
+
+                    <p>
+                        Some information about this general settings option
+                    </p>
+                </div>
+                <!-- /.form-group -->
+            </form>
+        </div>
+        <!-- /.tab-pane -->
+    </div>
+</aside>
+<!-- /.control-sidebar -->
